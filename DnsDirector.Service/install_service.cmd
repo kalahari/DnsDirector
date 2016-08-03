03ae0d3ebb462854145e6ed83f7980ccd284a11d
Blake Mitchell <kalahari@users.noreply.github.com>
2016-08-03 10:09:39 -0700

    install script now chagnes to correct directory and makes service auto-restart

--- DnsDirector.Service/install_service.cmd
+++ DnsDirector.Service/install_service.cmd
@@ -1,4 +1,6 @@
 echo "Installing DnsDirector as a service."
+cd "%~dp0"
 "%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe" DnsDirector.Service.exe
+sc failure "DnsDirector" reset= 300 actions= restart/3000/restart/3000/restart/3000
 net start DnsDirector
 timeout /t -1
